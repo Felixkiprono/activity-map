56888ec028ae8f1043e5fb3e8569024d6edefebe
kipronovich <felixkiptor@gmail.com>
2024-06-16 14:26:37 +0300

    added a new file

---
+++ includes/class-activity-map-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://activity-map/sergei
+ * @since      1.0.0
+ *
+ * @package    Activity_Map
+ * @subpackage Activity_Map/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Activity_Map
+ * @subpackage Activity_Map/includes
+ * @author     Sergei Kiprono <felixkipronovich@gmail.com>
+ */
+class Activity_Map_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
---
+++ public/partials/activity-map-public-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://activity-map/sergei
+ * @since      1.0.0
+ *
+ * @package    Activity_Map
+ * @subpackage Activity_Map/public/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
