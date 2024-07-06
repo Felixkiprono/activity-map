be6bcc51bdc92ab3d4ae9b3e87376004d4cc0ea5
kipronovich <felixkiptor@gmail.com>
2024-07-06 14:19:00 +0300

    added plugin events install,activate,deactivate and delete

--- classes/class-am-hooks.php
+++ classes/class-am-hooks.php
@@ -14,10 +14,12 @@ class AM_Hooks
 		include(plugin_dir_path(ACTIVITY_MAP__FILE__) . '/hooks/class-am-hook-users.php');
 		include(plugin_dir_path(ACTIVITY_MAP__FILE__) . '/hooks/class-am-hook-comments.php');
 		include(plugin_dir_path(ACTIVITY_MAP__FILE__) . '/hooks/class-am-hook-attachments.php');
+		include(plugin_dir_path(ACTIVITY_MAP__FILE__) . '/hooks/class-am-hook-plugins.php');
 
 		new Am_Hook_Posts();
 		new Am_Hook_Users();
 		new Am_Hook_Comments();
 		new Am_Hook_Attachments();
+		new Am_Hook_Plugins();
 	}
 }
